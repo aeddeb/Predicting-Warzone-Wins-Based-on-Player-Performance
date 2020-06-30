2f7065a5c5e47d35723d52fe7da7d5c40b303119
Ali Eddeb <ali.eddeb@gmail.com>
2020-06-30 17:21:36 -0400

    revising functions to place scraped data in dict

--- scraper_functions.py
+++ scraper_functions.py
@@ -7,8 +7,18 @@ Created on Mon Jun 22 15:43:55 2020
 Saving all functions for scraping in this py file
 """
 
+#lIBRARIES
+#------------------------------------------------------------------------------
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+#------------------------------------------------------------------------------
+
+
+#FUNCTIONS
+#------------------------------------------------------------------------------
 # 1. Extracting win data per player and saving it into a nested list
-def get_wins(data, platform, win_data):
+def get_stat(data, platform, feature, scraped_data):
     '''
     Get win data for each player from a scraped page from cod.tracker.gg.
     
@@ -18,29 +28,27 @@ def get_wins(data, platform, win_data):
         Contains the player info that was scraped from the wins leaderboard.
     platform: string
         Contains the name of the platform the players play on.
-    win_data : Nested List
-        Extracted elements are appended to this win_data list.
-        For each player in data, the following is extracted:
-            1. Username
-            2. Platform (e.g. playstation)
-            3. Wins
-            4. Matches Played
+    feature: string
+        Name of feature being scraped e.g. Wins.
+    win_data : Dictionary
+        Extracted elements are placed into this dictionary with the key being 
+        the player's username.
+        The associated value is a list containing the player's:
+            1. Platform (e.g. playstation)
+            2. Stat (e.g. Wins)
+            3. Matches Played (Only scraped if feature is Wins)
 
     Returns
     -------
-    win_data
+    scraped_data
     
-    Notes
-    -----
-    A header that can be used for win list: [['username','platform','wins','matches played']]
     '''
+    
+    assert isinstance(scraped_data, dict), 'scraped_data must be a dictionary'
     
     #for each player on the page...
     for player in range(len(data)):
         
-        #...get username, wins, matches played (mp) and platform
-        wins = int((data[player].find('td', attrs = {'class':'stat'}).text).replace(',',''))
-        mp = int((data[player].find('td', attrs = {'class':'stat collapse'}).text).replace(',',''))
         #for username, need to check if the player is on battlenet or not...
         if platform != 'battlenet':
             username = data[player].find('span', attrs = {'class':'trn-ign__username'}).text
@@ -52,9 +60,62 @@ def get_wins(data, platform, win_data):
                 username = name + player_id
             except:
                 username = name
-                
-    
-        #add the players info to the win_data list
-        win_data.append([username,platform,wins,mp])
+
+        if feature == 'Wins':
+            #...get username, wins, matches played (mp) and platform
+            wins = int((data[player].find('td', attrs = {'class':'stat'}).text).replace(',',''))
+            mp = int((data[player].find('td', attrs = {'class':'stat collapse'}).text).replace(',',''))
+            #add the players info to the win_data list
+            #win_data.append([username,platform,wins,mp])
+            scraped_data[username] = [platform, wins, mp]
+        else:
+            #...get other statistic
+            stat = data[player].find('td', attrs = {'class':'stat'}).text
+            scraped_data[username] = [platform, stat]
             
-    return win_data
+    return scraped_data
+
+
+
+def scrape_pages(platform, feature,  end, scraped_data, start=1):
+    '''
+    Cycle through pages of cod.tracker.gg and extract info.
+    
+    Parameters
+    ----------
+    platform: string
+        Contains the name of the platform the players play on.
+    feature: string
+        Name of feature being scraped e.g. Wins.
+    
+    Returns
+    -------
+    
+    '''
+    url = f"https://cod.tracker.gg/warzone/leaderboards/battle-royale/{platform}/{feature}?page="
+    
+    for num in tqdm(range(start,end)):
+
+        try:
+            #getting page number
+            page_num = str(num)
+    
+            page = requests.get(url + page_num)
+    
+            soup = BeautifulSoup(page.content, 'html.parser')
+    
+            # ----------------------------------------------------------------------------
+            # Need to select row and extract console, player name, wins and Matches Played
+    
+            #each row of player data has a tr tag
+            data = soup.find_all('tr')
+            #removing first row which is just the table heading
+            del data[0]
+            
+            scraped_data = get_stat(data, platform, feature, scraped_data)
+                
+        except:
+            print(f'Encountered error at batch {num}')
+            return scraped_data 
+        
+    return scraped_data
